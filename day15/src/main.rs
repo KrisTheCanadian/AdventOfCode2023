8a57b69555e36f231f3f3bf91cb42aa97e2562f2
Kris <kristhecanadian101@gmail.com>
2023-12-15 12:46:15 -0500

    day 15 part 2

--- day15/src/main.rs
+++ day15/src/main.rs
@@ -1,5 +1,82 @@
 fn main() {
     part1();
+    part2();
+}
+
+fn part2() {
+    let input = include_str!("./input.txt");
+    let strings: Vec<&str> = input.split(",").collect();
+    let hashmap = process_hashmap(strings);
+    print_hashmap(&hashmap);
+
+    let mut sum = 0;
+    for (k, v) in hashmap.iter().enumerate() {
+        for (i, t) in v.iter().enumerate() {
+            sum += ((k + 1) * (i + 1)) as i32 * t.1;
+        }
+    }
+    println!("Part 2 - sum: {}", sum);
+}
+
+fn print_hashmap(hashmap: &Vec<Vec<(String, i32)>>) {
+    for (i, v) in hashmap.iter().enumerate() {
+        if v.len() > 0 {
+            println!("{}: {:?}", i, v);
+        }
+    }
+}
+
+fn process_hashmap(strings: Vec<&str>) -> Vec<Vec<(String, i32)>> {
+    let mut hashmap: Vec<Vec<(String, i32)>> = Vec::new();
+    // set length of vector to 256
+    for _ in 0..256 {
+        hashmap.push(Vec::new());
+    }
+
+    for s in strings {
+        if s.contains("=") {
+            let parts: Vec<&str> = s.split("=").collect();
+            let label = parts[0];
+            let value: i32 = parts[1].parse().unwrap();
+            let hash = hash(label);
+
+            // check if label exists in vector
+            if hashmap[hash as usize].len() > 0 {
+                // look for the tuple with the label
+                let mut found = false;
+                for (i, v) in hashmap[hash as usize].iter().enumerate() {
+                    if v.0 == label {
+                        found = true;
+                        hashmap[hash as usize][i].1 = value;
+                        break;
+                    }
+                }
+
+                if !found {
+                    hashmap[hash as usize].push((label.to_string(), value));
+                }
+
+            } else {
+                hashmap[hash as usize].push((label.to_string(), value));
+            }
+
+        } else if s.contains("-") {
+            let parts: Vec<&str> = s.split("-").collect();
+            let label = parts[0];
+            let hash = hash(label);
+            // check if value exists in vector
+            if hashmap[hash as usize].len() > 0 {
+                for (i, v) in hashmap[hash as usize].iter().enumerate() {
+                    if v.0 == label {
+                        hashmap[hash as usize].remove(i);
+                        break;
+                    }
+                }
+            }
+
+        }
+    }
+    hashmap
 }
 
 fn part1() {
@@ -8,13 +85,13 @@ fn part1() {
 
     let mut sum = 0;
     for s in strings {
-        sum += hash_algorithm(s);
+        sum += hash(s);
     }
 
-    println!("sum: {}", sum);
+    println!("Part 1 - sum: {}", sum);
 }
 
-fn hash_algorithm(input: &str) -> i32 {
+fn hash(input: &str) -> i32 {
     let chars: Vec<char> = input.chars().collect();
     let mut current_value: i32 = 0;
     for c in chars {
