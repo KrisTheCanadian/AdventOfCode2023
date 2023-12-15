94866cac94894d8c5e2c51b54a7b92983bede0cb
Kris <kristhecanadian101@gmail.com>
2023-12-15 10:52:54 -0500

    day14 part 2 - WIP
    needs to be updated to find the cycle

--- day14/src/main.rs
+++ day14/src/main.rs
@@ -1,5 +1,37 @@
+use std::collections::HashMap;
+
 fn main() {
     part1();
+    part2();
+}
+
+fn part2() {
+    let input = include_str!("./input.txt");
+    let mut dish: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
+    print_dish(&dish);
+
+    for i in 0..1000 {
+        move_rocks_north(&mut dish);
+        move_rocks_west(&mut dish);
+        move_rocks_south(&mut dish);
+        move_rocks_east(&mut dish);
+    }
+
+
+    print_dish(&dish);
+    println!("Part2 - load: {}", calculate_load(&mut dish));
+}
+
+fn calculate_load(dish: &mut Vec<Vec<char>>) -> usize {
+    let mut load = 0;
+    for row in 0..dish.len() {
+        for col in 0..dish[0].len() {
+            if dish[row][col] == 'O' {
+                load += 1 * (dish.len() - row);
+            }
+        }
+    }
+    load
 }
 
 fn part1() {
@@ -7,36 +39,7 @@ fn part1() {
     let mut dish: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
     print_dish(&dish);
 
-    // move all the rocks north
-
-    // for each column
-    // move O up if there's a dot above it
-    let mut is_completed = false;
-    while !is_completed {
-        is_completed = true;
-        for col in 0..dish[0].len() {
-            for row in 0..dish.len() {
-                while dish[row][col] == 'O' && row > 0 && dish[row - 1][col] == '.' {
-                    dish[row][col] = '.';
-                    dish[row - 1][col] = 'O';
-                }
-            }
-        }
-
-        // check all 0 to see if there's a dot above it
-        for col in 0..dish[0].len() {
-            for row in 0..dish.len() {
-                if dish[row][col] == 'O' && row > 0 && dish[row - 1][col] == '.' {
-                    is_completed = false;
-                    break;
-                }
-            }
-        }
-
-        if is_completed {
-            break;
-        }
-    }
+    move_rocks_north(&mut dish);
 
     // calculate load
     let mut load = 0;
@@ -50,6 +53,85 @@ fn part1() {
 
     print_dish(&dish);
     println!("Part1 - load: {}", load);
+}
+
+fn move_rocks_east(dish: &mut Vec<Vec<char>>) {
+    let rows = dish.len();
+    let cols = dish[0].len();
+
+    let mut is_completed = false;
+    while !is_completed {
+        is_completed = true;
+        for row in 0..rows {
+            for col in (0..cols - 1).rev() { // iterate in reverse
+                if dish[row][col] == 'O' && dish[row][col + 1] == '.' {
+                    dish[row][col] = '.';
+                    dish[row][col + 1] = 'O';
+                    is_completed = false;
+                }
+            }
+        }
+    }
+}
+
+
+fn move_rocks_south(dish: &mut Vec<Vec<char>>) {
+    let rows = dish.len();
+    let cols = dish[0].len();
+
+    let mut is_completed = false;
+    while !is_completed {
+        is_completed = true;
+        for col in 0..cols {
+            for row in (0..rows - 1).rev() { // iterate in reverse
+                if dish[row][col] == 'O' && dish[row + 1][col] == '.' {
+                    dish[row][col] = '.';
+                    dish[row + 1][col] = 'O';
+                    is_completed = false;
+                }
+            }
+        }
+    }
+}
+
+
+fn move_rocks_west(dish: &mut Vec<Vec<char>>) {
+    let rows = dish.len();
+    let cols = dish[0].len();
+
+    let mut is_completed = false;
+    while !is_completed {
+        is_completed = true;
+        for row in 0..rows {
+            for col in 1..cols { // start from second column
+                if dish[row][col] == 'O' && dish[row][col - 1] == '.' {
+                    dish[row][col] = '.';
+                    dish[row][col - 1] = 'O';
+                    is_completed = false;
+                }
+            }
+        }
+    }
+}
+
+
+fn move_rocks_north(dish: &mut Vec<Vec<char>>) {
+    let rows = dish.len();
+    let cols = dish[0].len();
+
+    let mut is_completed = false;
+    while !is_completed {
+        is_completed = true;
+        for col in 0..cols {
+            for row in 1..rows { // start from the second row
+                if dish[row][col] == 'O' && dish[row - 1][col] == '.' {
+                    dish[row][col] = '.';
+                    dish[row - 1][col] = 'O';
+                    is_completed = false;
+                }
+            }
+        }
+    }
 }
 
 fn print_dish(dish: &Vec<Vec<char>>) {
